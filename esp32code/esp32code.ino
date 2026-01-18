b09c7fde7f7bc7158490bb1555d80d9ae6759fdb
Danylo Galytskyy <work.danylo.galytskyy@gmail.com>
2026-01-18 15:40:08 +0100

    works with sorting and possibly lighting

--- esp32code/esp32code.ino
+++ esp32code/esp32code.ino
@@ -16,6 +16,13 @@
 
 #include <WiFiManager.h>
 
+#include <WS2812FX.h>
+
+#define LED_PIN 5     // digital pin used to drive the LED strip
+#define LED_COUNT 25  // number of LEDs on the strip
+
+WS2812FX led_array = WS2812FX(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
 #define REGION_COUNT 27
 
 WiFiManager wm;
@@ -27,33 +34,33 @@ typedef struct {
 } Region;
 
 const Region regions[]{
-    { "Crimea", 0 },
-    { "Kherson", 22 },
-    { "Zaporizhzhia", 7 },
-    { "Donetsk", 4 },
-    { "Luhansk", 12 },
-    { "Kharkiv", 21 },
-    { "Dnipropetrovsk", 3 },
-    { "Mykolaiv", 14 },
-    { "Odesa", 15 },
-    { "Kirovohrad", 11 },
-    { "Poltava", 16 },
-    { "Sumy", 19 },
-    { "Chernihiv", 26 },
-    { "Kyiv", 10 },
-    { "Cherkasy", 24 },
-    { "Vinnytsia", 2 },
-    { "Zhytomyr", 5 },
-    { "Rivne", 17 },
-    { "Khmelnytsky", 23 },
-    { "Chernivtsi", 25 },
-    { "Ternopil", 20 },
-    { "Ivano-Frankivsk", 8 },
-    { "Zakarpattia", 6 },
-    { "Lviv", 13 },
-    { "Volyn", 1 },
-    { "Kyiv City", 9 },
-    { "Sevastopol City", 18 }
+  { "Crimea", 0 },
+  { "Kherson", 22 },
+  { "Zaporizhzhia", 7 },
+  { "Donetsk", 4 },
+  { "Luhansk", 12 },
+  { "Kharkiv", 21 },
+  { "Dnipropetrovsk", 3 },
+  { "Mykolaiv", 14 },
+  { "Odesa", 15 },
+  { "Kirovohrad", 11 },
+  { "Poltava", 16 },
+  { "Sumy", 19 },
+  { "Chernihiv", 26 },
+  { "Kyiv", 10 },
+  { "Cherkasy", 24 },
+  { "Vinnytsia", 2 },
+  { "Zhytomyr", 5 },
+  { "Rivne", 17 },
+  { "Khmelnytsky", 23 },
+  { "Chernivtsi", 25 },
+  { "Ternopil", 20 },
+  { "Ivano-Frankivsk", 8 },
+  { "Zakarpattia", 6 },
+  { "Lviv", 13 },
+  { "Volyn", 1 },
+  { "Kyiv City", 9 },
+  { "Sevastopol City", 18 }
 };
 
 
@@ -62,10 +69,10 @@ const Region regions[]{
 
 
 
-char unsorted_letters[REGION_COUNT+1];
+char unsorted_letters[REGION_COUNT + 1];
 
 
-char sorted_letters[REGION_COUNT+1];
+char sorted_letters[REGION_COUNT + 1];
 
 
 void setup() {
@@ -74,6 +81,10 @@ void setup() {
   url = "https://api.alerts.in.ua/v1/iot/active_air_raid_alerts_by_oblast.json?token=" + api_token;
 
   Serial.begin(115200);
+
+  led_array.init();
+  led_array.setBrightness(128);
+  led_array.start();
 }
 
 void loop() {
@@ -82,24 +93,57 @@ void loop() {
     sortData();
   }
 
-
+  displayData();
   delay(10000);
 }
 
-void sortData(){
+void displayData() {
+  for (int i = 0; i < LED_COUNT; i++) {
+    uint32_t c;
+    switch (sorted_letters[i]) {
+      case 'A':
+        c = RED;
+        break;
+      case 'P':
+        c = ORANGE;
+        break;
+      case 'N':
+        c = GREEN;
+        break;
+    }
+    Serial.print(regions[i].name);
+    Serial.print(" ");
+    Serial.println(sorted_letters[i]);
+    led_array.setPixelColor(i, c);
+    
+  }
 
-for(int i = 0; i< REGION_COUNT; i++){
-      sorted_letters[i] = unsorted_letters[(regions[i].string_index)];
-      Serial.print(i);
-      Serial.print(" ");
-      Serial.print(regions[i].string_index);
-      Serial.print(" ");
-      Serial.print(unsorted_letters[(regions[i].string_index)]);
-      Serial.print(" ");
-      Serial.println(sorted_letters); 
+  handleSpecial(27, 0, ORANGE);  // Crimea & Sevastopol
+  handleSpecial(26, 13, ORANGE); // Kyiv City & Kyiv Oblast
+
+  led_array.show();
 }
-Serial.println(" ");
-Serial.println(unsorted_letters);
-Serial.println(sorted_letters);
-Serial.println("");
+
+void handleSpecial(uint8_t idxA, uint8_t idxN, uint32_t color) {
+  if(sorted_letters[idxA] == 'A' && sorted_letters[idxN] == 'N') {
+    led_array.setPixelColor(idxN, color);
+  }
+}
+
+void sortData() {
+
+  for (int i = 0; i < REGION_COUNT; i++) {
+    sorted_letters[i] = unsorted_letters[(regions[i].string_index)];
+    Serial.print(i);
+    Serial.print(" ");
+    Serial.print(regions[i].string_index);
+    Serial.print(" ");
+    Serial.print(unsorted_letters[(regions[i].string_index)]);
+    Serial.print(" ");
+    Serial.println(sorted_letters);
+  }
+  Serial.println(" ");
+  Serial.println(unsorted_letters);
+  Serial.println(sorted_letters);
+  Serial.println("");
 }
